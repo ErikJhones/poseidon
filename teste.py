0ef9bde0567d68e3b0c38358510bb8a50bcf6c61
codeINnOUT <erik.jhones06@aluno.ifce.edu.br>
2026-02-19 13:38:14 -0300

    commit

--- teste.py
+++ teste.py
@@ -2,6 +2,7 @@ from tabicl import TabICLClassifier
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import time
 
 # 1. Carregar o dataset
 # Certifique-se de que o arquivo 'titanic.csv' esteja na mesma pasta do script
@@ -17,10 +18,14 @@ y = df['Survived']
 # random_state garante que a divisão seja a mesma toda vez que você rodar o código
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+ini = time.time()
 clf = TabICLClassifier()
 clf.fit(X_train, y_train)  # downloads checkpoint on first use, otherwise cheap
 print(clf.predict(X_test))  # in-context learning happens here
+fim = time.time()
 
+print(y_test)
+print(f"tempo: {fim-ini}")
 # reg = TabICLRegressor()
 # reg.fit(X_train, y_train)
 # reg.predict(X_test)
