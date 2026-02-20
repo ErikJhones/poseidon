4c4b1d375d48d6c26d5849b10d4c5e3117c44d8d
codeINnOUT <erik.jhones06@aluno.ifce.edu.br>
2026-02-20 12:40:49 -0300

    commit

--- test.py
+++ test.py
@@ -10,30 +10,40 @@ import os
 
 start = time.time()
 
-# Reprodutibilidade
-np.random.seed(42)
+# # Reprodutibilidade
+# np.random.seed(42)
 
-n_samples = 6400
-n_features = 100
-n_classes = 2
-# X_train: dados aleatórios contínuos
-X_train = pd.DataFrame(
-    np.random.randn(n_samples, n_features),
-    columns=[f"feature_{i}" for i in range(n_features)]
-)
-# y_train: classes inteiras de 0 a 9
-y_train = np.random.randint(0, n_classes, size=n_samples)
+# n_samples = 6400
+# n_features = 100
+# n_classes = 2
+# # X_train: dados aleatórios contínuos
+# X_train = pd.DataFrame(
+#     np.random.randn(n_samples, n_features),
+#     columns=[f"feature_{i}" for i in range(n_features)]
+# )
+# # y_train: classes inteiras de 0 a 9
+# y_train = np.random.randint(0, n_classes, size=n_samples)
 
-X_test = pd.DataFrame(
-    np.random.randn(2000, n_features),
-    columns=[f"feature_{i}" for i in range(n_features)]
-)
+# X_test = pd.DataFrame(
+#     np.random.randn(2000, n_features),
+#     columns=[f"feature_{i}" for i in range(n_features)]
+# )
 
-ini = time.time()
+X_train = np.load("N_train.npy")
+X_train = pd.DataFrame(X_train)
+X_test = np.load("N_test.npy")
+X_test = pd.DataFrame(X_test)
+y_train = np.load("y_train.npy")
+y_train = pd.DataFrame(y_train)
+y_test = np.load("y_test.npy")
+# y_test = pd.DataFrame(y_test)
+
 clf = TabICLClassifier(batch_size=16)
 clf.fit(X_train, y_train)  # downloads checkpoint on first use, otherwise cheap
 pred = clf.predict(X_test)  # in-context learning happens here
 
+accuracy = (y_test == pred).mean()
+print("Accuracy:", accuracy)
 
 end = time.time()
 elapsed = end - start
